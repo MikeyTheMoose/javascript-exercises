5e9db7ba8dfc73b81e3ac566d12461be3d5c09d0
Mikey <110850792+MikeyTheMoose@users.noreply.github.com>
2025-04-02 17:08:31 -0500

    Add formulae for temperature conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  return Math.round((fahr-32)*5/9 *10)/10;
+  return cels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return Math.round(((cels * 9/5)+32)*10)/10;
 };
 
 // Do not edit below this line
