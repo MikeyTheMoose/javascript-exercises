e1221e273257ed403d79e45f3c68c905ef0f613b
Mikey <110850792+MikeyTheMoose@users.noreply.github.com>
2025-04-02 16:56:07 -0500

    Write leapYears() code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
 
+    let isLeapYear = null;
+
+    // Leap years are divisible by 4, 
+    // but not by 100, except if also divisible by 400.
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {isLeapYear = true;}
+            else {isLeapYear = false;}
+        }
+        else {isLeapYear = true}
+    }
+    else isLeapYear = false;
+    return isLeapYear;
 };
 
 // Do not edit below this line
