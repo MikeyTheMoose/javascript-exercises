ed3903e38da619904a18612ecc63d2bfa7b8f446
Mikey <110850792+MikeyTheMoose@users.noreply.github.com>
2025-04-02 16:13:46 -0500

    Add code to removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...other) {
+    let newArray = [];
+    for (el of arr) {
+        if (!other.includes(el)) {newArray.push(el) }
+    }
+    return newArray;
+    
 };
 
 // Do not edit below this line
